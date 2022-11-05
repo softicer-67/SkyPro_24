3e60d8b67472b14e6c13c7cbed56fdcc26a198ea
Yuri <57116431+softicer-67@users.noreply.github.com>
2022-11-05 09:06:53 +0500

    Update models.py

--- models.py
+++ models.py
@@ -9,15 +9,3 @@ class Employees(db.Model):
     phone = db.Column(db.String(15))
 
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100))
-#     email = db.Column(db.String(100))
-#     phone = db.Column(db.Integer)
-#
-#     def __init__(self, *args, **kwargs):
-#         super(User, self).__init__(*args, **kwargs)
-#
-#     def __repr__(self):
-#         return f" id {self.id} user {self.name} email {self.email} phone {self.phone}"
-
